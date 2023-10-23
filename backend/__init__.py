ce3945e3198a2fb6c2dff699cedd16c0eafe46ba
Augusto <augustoaraujo2002@gmail.com>
2023-10-23 19:17:46 -0300

    flask API completed

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask_cors import CORS
 
+
 app = Flask(__name__)
 CORS(app)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:LagoPuelo99@localhost:3306/flask'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 import backend.views  # nopep8
