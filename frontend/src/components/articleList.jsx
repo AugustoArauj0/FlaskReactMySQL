55518bdff018e2a08ed364aba0171561628d5361
Augusto <augustoaraujo2002@gmail.com>
2023-10-24 19:37:28 -0300

    WIP

--- frontend/src/components/articleList.jsx
+++ frontend/src/components/articleList.jsx
@@ -8,12 +8,22 @@ function ArticleList(props) {
           return (
             <div key={article.id}>
               <br></br>
+              <hr></hr>
               <h3>
                 <strong>{article.title}</strong>
               </h3>
               <p>{article.body}</p>
               <p>Created At: {article.date} </p>
+              <div className="row">
+                <div className="col-md-1">
+                  <button className="btn btn-primary">Update</button>
+                </div>
+                <div className="col">
+                  <button className="btn btn-danger">Delete</button>
+                </div>
+              </div>
               <br></br>
+              <hr></hr>
             </div>
           );
         })}
